dc0afe7eb913e30861d856a7d4cdccc0435f1726
Jhon <JLIPAMAZA@GMAIL.com>
2022-05-10 22:56:35 -0500

    100522

--- CODE.R
+++ CODE.R
@@ -24,32 +24,35 @@ ATACKS$Category <- str_replace(ATACKS$Category,"Backdoors","Backdoor") #Reemplaz
 
 
 arrange(compare, desc(n))
-
 plot(compare)
+
 ATACKS$Attack.category <- NULL
 ATACKS$Attack.subcategory <- NULL
 ATACKS$Protocol <- NULL
+ATACKS$Attack.Reference <- NULL
+
 unique(ATACKS$Source.IP)
 unique(ATACKS$Source.Port)
 unique(ATACKS$Destination.IP)
 unique(ATACKS$Destination.Port)
 unique(ATACKS$Attack.Reference)
-ATACKS$Attack.Reference <- NULL
 
 
-ggplot(TCP, aes(fill=TCP$Source.IP, y=Protocol_new, x=Category)) + geom_bar(position="stack",stat='identity')
+
+TCP <- ATACKS[ATACKS$Protocolos == 'tcp', ] #Seleccionar el frame de acuerdo a un tipo de protocolo
+TCP$Protocol_new <- as.character(TCP$Protocolos)
 
 
 
 select(ATACKS,Category="Fuzzers")
-plot(compare)
+plot(compare_TCP)
 
-TCP <- ATACKS[ATACKS$Protocolos == 'tcp', ] #Seleccionar el frame de acuerto a un tipo de protocolo
+
 View(TCP)
 ATACKS[ATACKS$Protocolos == 'tcp',11 ]
 TCP <- ATACKS(Protocolos=="Fuzzers")
 View(TCP$Protocol_new)
-TCP$Protocol_new <- as.character(TCP$Protocolos)
+
 unique(TCP$Protocol_new)
 unique(TCP$Protocolos)
 unique(ATACKS$Subcategory)
@@ -57,3 +60,16 @@ plot(TCP$Source.IP)
 
 compare_TCP <- TCP %>% group_by(Source.IP,Protocol_new) %>% summarise ( n=n())
 arrange(compare_TCP, desc(n))
+
+#Graficas
+
+ggplot(TCP, aes(fill=TCP$Source.IP, y=Protocol_new, x=Category)) + geom_bar(position="stack",stat='identity')
+P <- ggplot(TCP, aes(x = Category, y = Protocol_new, colour=Source.IP))
+P <- P + geom_point(aes(shape = Source.IP))
+P <- P + xlab("ATAQUES CIBERNETICOS") + ylab(" PROTOCOLOS") + ggtitle("ATAQUES CIBERNETICOS VS PROTOCOLOS  CON ORIGEN DE IP")
+P <- P + geom_smooth(method = "lm")
+P <- P + facet_grid( Source.IP  ~.)
+
+
+
+
